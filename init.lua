f6c4524ea04c10d748348b600231b101100e9ba9
Celso Fernandes <celso.fernandes@gmail.com>
2022-10-19 12:39:51 -0300

    yank

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 -- use this file in case need additional config
 vim.cmd("au BufNewFile,BufRead *.cr			setf crystal")
 vim.cmd("au BufNewFile,BufRead *.rake			setf ruby")
+
+vim.cmd("source yank.vim")
