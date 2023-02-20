f7f4c6e1161dfc600e139fa80418183ca01dddd9
Celso Fernandes <celso.fernandes@gmail.com>
2023-02-20 09:23:24 -0300

    ruby syntax tree and sorbet

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -1,6 +1,7 @@
 -- custom.plugins.lspconfig
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
+local util = require 'lspconfig.util'
 
 local lspconfig = require "lspconfig"
 local servers = { "html", "cssls" }
@@ -12,13 +13,21 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+require'lspconfig'.syntax_tree.setup{}
+require'lspconfig'.sorbet.setup{}
+
 lspconfig.solargraph.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
     useBundler = true
+  },
+  init_options = { formatting = true },
+  flags = {
+    debounce_text_changes = 150
   }
 }
+
 -- lspconfig.scry.setup { }
 lspconfig.crystalline.setup{
   on_attach = on_attach,
